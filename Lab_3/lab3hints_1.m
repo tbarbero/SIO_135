d6c1628ffb17b57f6619f87cc708b3acccd29c56
Tyler <tbarbero@ucsd.edu>
2020-01-15 22:46:17 -0800

    removing unnecessary directories

--- Lab_3/lab3hints_1.m
+++
@@ -1,3 +0,0 @@
-% load the topography image
-fid = fopen('lajolla_swab','r');
-topo = fread(fid,[3240,1440],'int16')';
--- Lab_3/lab3hints_2.m
+++
@@ -1,4 +0,0 @@
-% load the JPEG image
-z = imread('Paraglider.jpg');
-% extract the red component
-r = z(:,:,1);
